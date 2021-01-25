d4e322d38c60a41512d48b4f04d66367bf31386d
Pedro Puccini <puccinidesign@gmail.com>
2021-01-24 21:41:06 -0300

    tercer commit

--- js/carrito.js
+++ js/carrito.js
@@ -1,12 +1,27 @@
-// Definición de la función
-function sumaCarrito(producto1, producto2) {
-  var resultado = producto1 + producto2;
-  alert("Cantidad total de productos a comprar " + resultado);
+function Producto(nombre, tipo, precio) {
+  this.nombre = nombre;
+  this.tipo = tipo;
+  this.precio = precio;
 }
 
-// Declaración de las variables
-var numero1 = parseInt(prompt("Cuántos unidades del producto 1 desea comprar? "));
-var numero2 = parseInt(prompt("Cuántos unidades del producto 2 desea comprar? "));
+let productos = []
 
-// Llamada a la función
-sumaCarrito(numero1, numero2);
+productos.push(new Producto("Producto 1", "Cucha", 9800))
+productos.push(new Producto("Producto 2", "Cama", 6700))
+productos.push(new Producto("Producto 3", "Rascador", 3600))
+productos.push(new Producto("Producto 4", "Comedero", 2900))
+
+
+for (let i = 0; i < productos.length; i++) {
+console.log(productos[i])
+}
+
+// Selección de producto
+
+console.log(productos.filter((el) => el.tipo == "Cucha"))
+
+
+var comprar = parseInt(prompt("Cuántos unidades del producto desea comprar? "));
+console.log("Precio final de la compra " + comprar);
+
+// Luego quiero poder multiplicar el precio del producto por la cantidad seleccionada para optener el precio final de compra. No supe como hacerlo!
