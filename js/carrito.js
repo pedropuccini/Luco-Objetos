3bc5099ab8fc6e67d41a680bdcd433ffdf375d6e
Pedro Puccini <puccinidesign@gmail.com>
2021-02-06 14:02:43 -0300

    Segunda entrega

--- js/carrito.js
+++ js/carrito.js
@@ -1,27 +1,74 @@
-function Producto(nombre, tipo, precio) {
-  this.nombre = nombre;
-  this.tipo = tipo;
-  this.precio = precio;
+
+let carritoDeCompras = []
+
+const contenedorProductos = document.getElementById('contenedor-productos')
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('precioTotal')
+
+const selecTipo = document.getElementById('selecTipo')
+
+selecTipo.addEventListener('change', ()=>{
+    console.log(selecTipo.value)
+    if (selecTipo.value == "all") {
+        mostrarProductos(stockProductos)
+    } else {
+        mostrarProductos(stockProductos.filter((el)=> el.tipo == selecTipo.value))
+    }
+
+})
+
+
+mostrarProductos(stockProductos)
+
+function mostrarProductos(array){
+    contenedorProductos.innerHTML = ''
+    array.forEach((productoNuevo)=>{
+        let div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML += `
+                    <img src=${productoNuevo.img} alt="">
+                    <h3>${productoNuevo.nombre}</h3>
+                    <p>${productoNuevo.desc}</p>
+                    <p class="precioProducto">Precio: $${productoNuevo.precio}</p>
+                    <button id="boton${productoNuevo.id}" class="boton-agregar">Agregar a carrito <i class="fas fa-shopping-cart"></i></button>
+        `
+        contenedorProductos.appendChild(div)
+        
+        let boton = document.getElementById(`boton${productoNuevo.id}`)
+        
+        boton.addEventListener('click', ()=>{
+            agregarAlCarrito(productoNuevo.id)
+         })
+    })
 }
 
-let productos = []
 
-productos.push(new Producto("Producto 1", "Cucha", 9800))
-productos.push(new Producto("Producto 2", "Cama", 6700))
-productos.push(new Producto("Producto 3", "Rascador", 3600))
-productos.push(new Producto("Producto 4", "Comedero", 2900))
+function agregarAlCarrito(id) {
+    let productoAgregar = stockProductos.filter((el) => el.id == id)[0]
+    carritoDeCompras.push(productoAgregar)
+    actualizarCarrito()
+    
 
+    let div = document.createElement('div')
+    div.classList.add('productoEnCarrito')
+    div.innerHTML = `
+        <p>${productoAgregar.nombre}</p>
+        <p>Precio: $${productoAgregar.precio}</p>
+        <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+    `
+    contenedorCarrito.appendChild(div)
+    let botonEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
 
-for (let i = 0; i < productos.length; i++) {
-console.log(productos[i])
+    botonEliminar.addEventListener('click', ()=>{
+        botonEliminar.parentElement.remove()
+        carritoDeCompras = carritoDeCompras.filter((el) => el.id != productoAgregar.id)      
+        actualizarCarrito()
+    })
 }
 
-// Selección de producto
-
-console.log(productos.filter((el) => el.tipo == "Cucha"))
-
-
-var comprar = parseInt(prompt("Cuántos unidades del producto desea comprar? "));
-console.log("Precio final de la compra " + comprar);
-
-// Luego quiero poder multiplicar el precio del producto por la cantidad seleccionada para optener el precio final de compra. No supe como hacerlo!
+function actualizarCarrito() {
+    contadorCarrito.innerText = carritoDeCompras.length
+    precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
+}
