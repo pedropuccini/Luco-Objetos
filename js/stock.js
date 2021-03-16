8666c5de422c67baf6980d121fa1da427e7449e0
Pedro Puccini <puccinidesign@gmail.com>
2021-03-16 13:51:48 -0300

    Entrega Final Correccion

--- js/stock.js
+++
@@ -1,6 +0,0 @@
-let stockProductos = [
-    {id: 1, nombre: "Cucha", tipo: "cuchas", desc: "Mobiliario para tu mascota", precio: 12500, img: 'image/pufuProducto1.jpg'},
-    {id: 2, nombre: "Cama", tipo: "camas", desc: "Mobiliario para tu mascota", precio: 6900, img: 'image/pufuProducto2.jpg'},
-    {id: 3, nombre: "Rascador", tipo: "rascadores", desc: "Mobiliario para tu mascota", precio: 4800, img: 'image/pufuProducto3.jpg'},
-    {id: 4, nombre: "Comedero", tipo: "comederos", desc: "Mobiliario para tu mascota", precio: 3200, img: 'image/pufuProducto4.jpg'},
-]
